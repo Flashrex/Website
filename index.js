d05f0ac843b20e4def025f171bb9fbb251ec1581
Flashrex <dominik.duda96@gmx.de>
2019-06-08 16:58:34 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let blogposts = [
+    { imgpath: "images/hackerman.jpg", headline: "Java Grundlagen Tutorial", language: "Language: german/deutsch", body: "In diesem Tutorial erkläre ich die Grundlagen der objektorientierten Programmiersprache Java. Wir beginnen mit dem klassichen 'Hello World' und machen uns mit Variablen und Methoden vertraut. <br> Das Tutorial richtet sich an Einsteiger, die gerne in die Welt der Nullen und Einsen eintauchen möchten."},
+    { imgpath: "images/logo.jpg", headline: "Game of Thrones",  language: "Language: english/englisch", body: "In this post I talk about the famous HBO tv show 'Game of Thrones'. I give my opinion about the final season and the whole series and why I think its one of the best shows in the world and what I think they did wrong in the end."}
+];
+
+addBlogPost();
+function addBlogPost() {
+    let gridbox = document.getElementById("gridbox");
+    
+    for(var i = 0; i < blogposts.length; i++) {
+        gridbox.innerHTML += '<div class="blogpost" id="blogpost_' +i +'"' +'>'
+        +'<img src=' +blogposts[i].imgpath +' alt=img_' +i +' width="190" height="190"> '
+        +'<h3>' +blogposts[i].headline +'</h3>'
+        +'<h5>' +blogposts[i].language +'</h5>'
+        +'<p>' +blogposts[i].body +'</p>'
+        +'</div>';
+
+        document.getElementById("blogpost_" +i).style = 'grid-column: 1/2; grid-row: ' +(i+1) +'/' +(i+2) +';'; 
+    }
+}
---
+++ projects.js
@@ -0,0 +1,37 @@
+let Projects = [
+    { logopath: "images/snake.png", headline: "Snake", file: "games/snake/index.html", link: "https://github.com/", description: "Snake Game written in vanilla Javascript. You can leave and reenter the field. ;)"},
+    { logopath: "images/pong.png", headline: "Pong!", file: "games/pong/index.html", link: "https://github.com/", description: "Classic old Pong! written in vanilla Javascript. Added 2 Player Mode and Game Stages for extra difficulty."},
+    { logopath: "images/rage.png", headline: "San Street Life | RageMP", file: "", link: "https://github.com/Flashrex/San-Street-Life", description: "This is a gamemode written in C#, Html/Css and Js for the Gta V modification Rage:Mp. It is still work in progress."},
+    { logopath: "images/asteroid.png", headline: "AsteroidField", file: "", link: "https://gitlab.informatik.hs-fulda.de/fdai5902/raumschiff", description: "Our own version of the popular game 'Asteroids' from 1979 written in Java that we created as a university project."},
+]
+
+addGames();
+function addGames() {
+    let projectbox = document.getElementById("projectbox");
+
+    var counter = 1;
+    for(var i = 0; i < Projects.length; i++) {
+        let linkText = "Open on Github";
+        if(Projects[i].link.includes("gitlab")) linkText = "Open on Gitlab";
+        if(Projects[i].file !== "") {
+            projectbox.innerHTML += '<div class="project" id="project_' +i +'">'
+            +'<img src=' +Projects[i].logopath +' alt=img_' +i +'" width="100" height="100"> '
+            +'<h4>' +Projects[i].headline +'</h4>'
+            +'<span><a href="' +Projects[i].file +'">Play!</a></span> <span><a href="' +Projects[i].link +'">' +linkText +'</a></span>'
+            +'<p>' +Projects[i].description + '</p>'
+            +'</div>';
+        } else {
+            projectbox.innerHTML += '<div class="project" id="project_' +i +'">'
+            +'<img src=' +Projects[i].logopath +' alt=img_' +i +'" width="100" height="100"> '
+            +'<h4>' +Projects[i].headline +'</h4>'
+            +'<span><a href="' +Projects[i].link +'">' +linkText +'</a></span>'
+            +'<p>' +Projects[i].description + '</p>'
+            +'</div>';
+        }
+        
+
+        document.getElementById("project_" +i).style = 'grid-column:' +((i+1) % 4) +'/' +((i+2) % 4) +'; grid-row:' +counter +'/' +(counter+1);
+        if(i % 4 === 3) counter++;
+    }
+    
+}
